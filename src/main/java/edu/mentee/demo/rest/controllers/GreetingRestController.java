11ff41b0689a4c6645eba5123eb0f301526fc7e7
Rodrigo Diaz <esteban.diaz@globant.com>
2019-04-02 16:51:18 -0300

    Se quitan imports no utilizados y anotacion innecesaria por el momento

--- src/main/java/edu/mentee/demo/rest/controllers/GreetingRestController.java
+++ src/main/java/edu/mentee/demo/rest/controllers/GreetingRestController.java
@@ -3,20 +3,16 @@ package edu.mentee.demo.rest.controllers;
 import edu.mentee.demo.domain.Persona;
 import edu.mentee.demo.rest.responses.GenericResponse;
 import edu.mentee.demo.rest.responses.ServiceStatus;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/say-rest")
 public class GreetingRestController {
 
-    @ApiOperation(value = "Short description of what does", response = GenericResponse.class)
     @GetMapping(value = "/greeting", produces = MediaType.APPLICATION_JSON_VALUE)
     public GenericResponse sayGreeting() {
         ServiceStatus status = new ServiceStatus(HttpStatus.OK.value(), "Success");
